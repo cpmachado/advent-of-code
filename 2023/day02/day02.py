da90aa44d9a2e6f15c0a81b090ba5518b29c92bf
cpmachado <cpmachado@protonmail.com>
2023-12-02 22:32:10 +0000

    Solution for day 2

--- 2023/day02/day02.py
+++ 2023/day02/day02.py
@@ -4,40 +4,80 @@
 """
 import argparse
 from dataclasses import dataclass
+from functools import reduce
+from operator import mul
 
 
 @dataclass
 class CubeBag:
+    """
+    Describes a bag of cubes
+    """
     cubes: dict[str, int]
 
     @staticmethod
     def from_line(line: str):
-        cubes = dict()
+        """
+        Builds a Cube Bag from a record from the file
+        """
+        cubes = {}
         for record in line.split(","):
-            v, k = record.strip().split(" ")
-            cubes[k] = int(v)
+            val, k = record.strip().split(" ")
+            cubes[k] = int(val)
         return CubeBag(cubes)
 
     def has_enough(self, other):
+        """
+        Checks if a given bag could have been used from a draw of another
+        """
         return all(
             k in self.cubes and self.cubes[k] >= v for k, v in other.cubes.items()
+        )
+
+    def minimal_common_bag(self, other):
+        """
+        Computes the minimal bag, that contains enough cubes in both
+        """
+        return CubeBag(
+            dict(
+                (k, max(self.cubes.get(k, 0), other.cubes.get(k, 0)))
+                for k in set((*self.cubes.keys(), *other.cubes.keys()))
+            )
         )
 
 
 @dataclass
 class CubeGame:
+    """
+    Describes a game of cubes
+    """
     game_id: int
     bags: list[CubeBag]
 
     @staticmethod
     def from_line(line: str):
+        """
+        Builds a Cube Game from a line of the file
+        """
         game_data, cube_data = line.strip().split(":")
         _, game_id = game_data.split(" ")
         bags = [CubeBag.from_line(record) for record in cube_data.split(";")]
         return CubeGame(int(game_id), bags)
 
     def is_valid(self, bag: CubeBag) -> bool:
+        """
+        Checks if it is a valid game, based on a reference bag
+        """
         return all(bag.has_enough(bagi) for bagi in self.bags)
+
+    def power_set(self):
+        """
+        Computes the power set of the game
+        """
+        first, *rest = self.bags
+        return reduce(
+            mul, reduce(CubeBag.minimal_common_bag, rest, first).cubes.values(), 1
+        )
 
 
 def part1(filename: str, bag: CubeBag) -> int:
@@ -53,6 +93,14 @@ def part1(filename: str, bag: CubeBag) -> int:
         )
 
 
+def part2(filename: str) -> int:
+    """
+    Solution of part 2
+    """
+    with open(filename, encoding="utf8") as file_handle:
+        return sum(CubeGame.from_line(line).power_set() for line in file_handle)
+
+
 def main():
     """
     Main function of day 02 of Advent of code 2023
@@ -64,9 +112,11 @@ def main():
     parser.add_argument("-g", "--green", action="store", type=int, default=0)
     parser.add_argument("-b", "--blue", action="store", type=int, default=0)
     args = parser.parse_args()
-    bag = CubeBag({"red": args.red, "green": args.green, "blue": args.blue})
     if args.part == 1:
+        bag = CubeBag({"red": args.red, "green": args.green, "blue": args.blue})
         print(part1(args.file, bag))
+    elif args.part == 2:
+        print(part2(args.file))
     else:
         raise NotImplementedError("Not implemented")
 
--- 2023/day02/test_day02.py
+++ 2023/day02/test_day02.py
@@ -4,9 +4,9 @@
 """
 import os
 import unittest
-from day02 import part1, CubeBag
+from day02 import part1, part2, CubeBag
 
-TEST_FILE_PART1 = os.path.join("data", "test.txt")
+TEST_FILE = os.path.join("data", "test.txt")
 
 
 class Day02Tests(unittest.TestCase):
@@ -19,12 +19,24 @@ class Day02Tests(unittest.TestCase):
         Tests for day 2 part 1
         """
         bag = CubeBag({"red": 12, "green": 13, "blue": 14})
-        actual = part1(TEST_FILE_PART1, bag)
+        actual = part1(TEST_FILE, bag)
         expected = 8
         self.assertEqual(
             actual,
             expected,
             f"Expected valid game id sum to be {expected}, not {actual}",
+        )
+
+    def test_part2(self):
+        """
+        Tests for day 2 part 2
+        """
+        actual = part2(TEST_FILE)
+        expected = 2286
+        self.assertEqual(
+            actual,
+            expected,
+            f"Expected power set sum to be {expected}, not {actual}",
         )
 
 
