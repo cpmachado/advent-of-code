59726c082b02e0fb3365bb058ec3e8e9b45b1e5d
cpmachado <cpmachado@protonmail.com>
2023-12-02 00:26:20 +0000

    Solution for day 1

--- 2023/day01/test_day01.py
+++ 2023/day01/test_day01.py
@@ -1,17 +1,40 @@
+"""
+    Tests for day 01 of Advent of Code 2023
+    See LICENSE for details
+"""
 import os
 import unittest
-from day01 import part1
+from day01 import part1, part2
 
-TEST_FILE = os.path.join("data", "test_part1.txt")
+TEST_FILE_PART1 = os.path.join("data", "test_part1.txt")
+TEST_FILE_PART2 = os.path.join("data", "test_part2.txt")
 
 
 class Day01Tests(unittest.TestCase):
+    """
+    Tests for day 1
+    """
+
     def test_part1(self):
-        actual = part1(TEST_FILE)
+        """
+        Tests for day 1 part 1
+        """
+        actual = part1(TEST_FILE_PART1)
         expected = 142
         self.assertEqual(
             actual, expected, f"Expected calibration values {expected}, not {actual}"
         )
 
-if __name__ == '__main__':
+    def test_part2(self):
+        """
+        Tests for day 1 part 2
+        """
+        actual = part2(TEST_FILE_PART2)
+        expected = 281
+        self.assertEqual(
+            actual, expected, f"Expected calibration values {expected}, not {actual}"
+        )
+
+
+if __name__ == "__main__":
     unittest.main()
