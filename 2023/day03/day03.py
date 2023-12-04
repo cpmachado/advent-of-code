1b32750bffad053a5984b8e9d9cb4a47e2406747
cpmachado <cpmachado@protonmail.com>
2023-12-04 16:52:47 +0000

    Part 1

--- 2023/day03/day03.py
+++ 2023/day03/day03.py
@@ -3,15 +3,76 @@
     See LICENSE for details.
 """
 import argparse
+from dataclasses import dataclass
+from enum import Enum
+from itertools import takewhile
+
+
+@dataclass
+class Coordinate:
+    coord: list[int]
+
+    def __add__(self, other):
+        return Coordinate([x + y for x, y in zip(self.coord, other.coord)])
+
+    def is_adjacent(self, other) -> bool:
+        return all(abs(x - y) <= 1 for x, y in zip(self.coord, other.coord))
+
+
+@dataclass
+class EngineSchematic:
+    parts: list[tuple[Coordinate, str]]
+    symbols: list[tuple[Coordinate, str]]
+
+    def add_line(self, j: int, line: str):
+        relevant_chars = [
+            (
+                Coordinate([i, j]),
+                line[i]
+                if not line[i].isdigit()
+                else "".join(takewhile(str.isdigit, line[i:])),
+            )
+            for i, c in enumerate(line)
+            if c != "."
+            and (
+                i < 1
+                or line[i - 1] == "."
+                or c.isdigit()
+                and not line[i - 1].isdigit()
+                or not c.isdigit()
+                and line[i - 1].isdigit()
+            )
+        ]
+        self.parts += [
+            (coord, content) for coord, content in relevant_chars if content.isdigit()
+        ]
+        self.symbols += [
+            coord for coord, content in relevant_chars if not content.isdigit()
+        ]
+
+    def compute_sum(self):
+        return sum(
+            int(val)
+            for coord, val in self.parts
+            if any(
+                any(
+                    (coord + Coordinate([i, 0])).is_adjacent(coord_symb)
+                    for i in range(len(val))
+                )
+                for coord_symb in self.symbols
+            )
+        )
+
 
 def part1(filename: str) -> int:
     """
     Solution of part 1
     """
+    schematic = EngineSchematic([], [])
     with open(filename, encoding="utf8") as file_handle:
         for j, line in enumerate(file_handle):
-            pass
-    return 0
+            schematic.add_line(j, line.strip())
+    return schematic.compute_sum()
 
 
 def main():
