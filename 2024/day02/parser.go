51294669a40f34e55903b61b79a19e168935b7b9
cpmachado <cpmachado@protonmail.com>
2024-12-07 22:18:39 +0000

    day02.1: Parser and test passing

---
+++ 2024/day02/parser.go
@@ -0,0 +1,35 @@
+package day02
+
+import (
+	"bufio"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+// Parses the input file for day02
+func ParseFile(filename string) []Report {
+
+	file, err := os.Open(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+
+	result := []Report{}
+	for scanner.Scan() {
+		line := scanner.Text()
+		cols := strings.Fields(line)
+		report := Report{}
+
+		for _, col := range cols {
+			level, _ := strconv.Atoi(col)
+			report = append(report, Level(level))
+		}
+
+		result = append(result, report)
+	}
+	return result
+}
