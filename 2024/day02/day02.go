cb12d2d2fffc06de8d19b396509d6389537915f5
cpmachado <cpmachado@protonmail.com>
2024-12-07 21:53:03 +0000

    day02.1: Passing test solution

--- 2024/day02/day02.go
+++ 2024/day02/day02.go
@@ -1,9 +1,43 @@
 package day02
 
+const (
+	// Levels must differ by at least 1
+	MinLevelDifference = 1
+	// Levels must differ at most 3
+	MaxLevelDifference = 3
+)
+
 func SafeReport(report []int) bool {
-	return false
+	if len(report) < 3 {
+		return true
+	}
+
+	increasing := report[0] < report[1]
+	prev := report[0]
+
+	for i := 1; i < len(report); i++ {
+		curr := report[i]
+		if increasing {
+			if (prev+MinLevelDifference) > curr || (prev+MaxLevelDifference) < curr {
+				return false
+			}
+		} else {
+			if (prev-MinLevelDifference) < curr || (prev-MaxLevelDifference) > curr {
+				return false
+			}
+		}
+		prev = curr
+	}
+
+	return true
 }
 
 func CountSafeReports(reports [][]int) int {
-	return 0
+	count := 0
+	for _, report := range reports {
+		if SafeReport(report) {
+			count++
+		}
+	}
+	return count
 }
