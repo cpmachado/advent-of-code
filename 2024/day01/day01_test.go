4b7557934ba04bf4bf67821f5f430d07f86f1d84
cpmachado <cpmachado@protonmail.com>
2024-12-07 20:59:11 +0000

    day01.2: Add test ListSimilarityScore

--- 2024/day01/day01_test.go
+++ 2024/day01/day01_test.go
@@ -15,3 +15,16 @@ func TestListDiff(t *testing.T) {
 		t.Fatalf("Expected difference to be '%d' but got '%d'", want, got)
 	}
 }
+
+func TestListSimilarityScore(t *testing.T) {
+	// List of Ids
+	listA := []LocationId{3, 4, 2, 1, 3, 3}
+	// Other List of Ids
+	listB := []LocationId{4, 3, 5, 3, 9, 3}
+	got := ListSimilarityScore(listA, listB)
+	want := 31
+
+	if got != want {
+		t.Fatalf("Expected difference to be '%d' but got '%d'", want, got)
+	}
+}
