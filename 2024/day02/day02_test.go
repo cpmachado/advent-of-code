9e883fb12efcf2c721c3b6a71ca5481febf12e02
cpmachado <cpmachado@protonmail.com>
2024-12-07 23:05:41 +0000

    day02.2: Refactor tests to support dampener usage

--- 2024/day02/day02_test.go
+++ 2024/day02/day02_test.go
@@ -9,25 +9,34 @@ var mapSafe = map[bool]string{
 
 func TestReportIsSafe(t *testing.T) {
 	tests := []struct {
-		report Report
-		want   bool
+		report   Report
+		dampener bool
+		want     bool
 	}{
-		{report: Report{7, 6, 4, 2, 1}, want: true},
-		{report: Report{1, 2, 7, 8, 9}, want: false},
-		{report: Report{9, 7, 6, 2, 1}, want: false},
-		{report: Report{1, 3, 2, 4, 5}, want: false},
-		{report: Report{8, 6, 4, 4, 1}, want: false},
-		{report: Report{1, 3, 6, 7, 9}, want: true},
+		{report: Report{7, 6, 4, 2, 1}, dampener: false, want: true},
+		{report: Report{1, 2, 7, 8, 9}, dampener: false, want: false},
+		{report: Report{9, 7, 6, 2, 1}, dampener: false, want: false},
+		{report: Report{1, 3, 2, 4, 5}, dampener: false, want: false},
+		{report: Report{8, 6, 4, 4, 1}, dampener: false, want: false},
+		{report: Report{1, 3, 6, 7, 9}, dampener: false, want: true},
+		{report: Report{7, 6, 4, 2, 1}, dampener: true, want: true},
+		{report: Report{1, 2, 7, 8, 9}, dampener: true, want: false},
+		{report: Report{9, 7, 6, 2, 1}, dampener: true, want: false},
+		{report: Report{1, 3, 2, 4, 5}, dampener: true, want: true},
+		{report: Report{8, 6, 4, 4, 1}, dampener: true, want: true},
+		{report: Report{1, 3, 6, 7, 9}, dampener: true, want: true},
 	}
 
 	for _, test := range tests {
 		report := test.report
 
-		got := report.IsSafe()
+		got := report.IsSafe(test.dampener)
 		want := test.want
 
 		if got != want {
-			t.Fatalf("Expected %s report(%v), but got %s", mapSafe[want], report, mapSafe[got])
+			t.Fatalf(
+				"Expected %s report(%v) and dampener(%v), but got %s",
+				mapSafe[want], report, test.dampener, mapSafe[got])
 		}
 	}
 }
@@ -42,9 +51,23 @@ func TestCountSafeReports(t *testing.T) {
 		Report{1, 3, 6, 7, 9},
 	}
 
-	got := CountSafeReports(reports)
-	want := 2
-	if got != want {
-		t.Fatalf("Expected '%d' safe reports, but got '%d'", want, got)
+	tests := []struct {
+		description string
+		dampener    bool
+		want        int
+	}{
+		{description: "count safe reports without dampener", dampener: false, want: 2},
+		{description: "count safe reports with dampener", dampener: true, want: 4},
 	}
+
+	for _, test := range tests {
+		t.Run(test.description, func(t *testing.T) {
+			got := CountSafeReports(reports, test.dampener)
+			want := test.want
+			if got != want {
+				t.Fatalf("Expected '%d' safe reports, but got '%d'", want, got)
+			}
+		})
+	}
+
 }
