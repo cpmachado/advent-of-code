b8b408d54e29e439f52feb9ec1e531165596aa67
cpmachado <cpmachado@protonmail.com>
2024-12-07 21:02:43 +0000

    day01.2: Add test solution

--- 2024/day01/day01.go
+++ 2024/day01/day01.go
@@ -25,3 +25,22 @@ func ListDiff(lst, other []LocationId) int {
 
 	return sum
 }
+
+func ListSimilarityScore(lst, other []LocationId) int {
+	if len(lst) != len(other) {
+		log.Fatal("There's an assumption of the lists being the same size\n")
+	}
+	counter := make(map[LocationId]int)
+
+	// Compute count of second list
+	for _, v := range other {
+		counter[v]++
+	}
+	sum := 0
+
+	for _, v := range lst {
+		sum += int(v) * counter[v]
+	}
+
+	return sum
+}
