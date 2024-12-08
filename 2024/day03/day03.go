168b96b32dae0c465d4a2945bf12b67404bb6c18
cpmachado <cpmachado@protonmail.com>
2024-12-08 19:50:01 +0000

    day03.1: green light on trivial test

--- 2024/day03/day03.go
+++ 2024/day03/day03.go
@@ -1,5 +1,21 @@
 package day03
 
+import (
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+// Processes the multiplications from corrupted input program
 func ProgramInterpreter(input string) int {
-	return 0
+	sum := 0
+	re := regexp.MustCompile(`mul\(\d{1,3},\d{1,3}\)`)
+	cleanRe := regexp.MustCompile("[mul()]")
+	for _, v := range re.FindAllString(input, -1) {
+		cv := strings.Split(cleanRe.ReplaceAllString(v, ""), ",")
+		a, _ := strconv.Atoi(cv[0])
+		b, _ := strconv.Atoi(cv[1])
+		sum += a * b
+	}
+	return sum
 }
