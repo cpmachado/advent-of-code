94f15d7520f4b5d8e484961d5dc53965a51fa3b3
cpmachado <cpmachado@protonmail.com>
2024-12-08 22:47:12 +0000

    day03.2: First green light test

--- 2024/day03/day03.go
+++ 2024/day03/day03.go
@@ -6,10 +6,16 @@ import (
 	"strings"
 )
 
+const (
+	MulRegex  = `mul\(\d{1,3},\d{1,3}\)`
+	DoRegex   = `do\(\)`
+	DontRegex = `don't\(\)`
+)
+
 // Processes the multiplications from corrupted input program
 func ProgramInterpreter(input string) int {
 	sum := 0
-	re := regexp.MustCompile(`mul\(\d{1,3},\d{1,3}\)`)
+	re := regexp.MustCompile(MulRegex)
 	cleanRe := regexp.MustCompile("[mul()]")
 	for _, v := range re.FindAllString(input, -1) {
 		cv := strings.Split(cleanRe.ReplaceAllString(v, ""), ",")
@@ -22,5 +28,18 @@ func ProgramInterpreter(input string) int {
 
 // Improved processes the multiplications from corrupted input program
 func ProgramInterpreterImproved(input string) int {
-	return 0
+	re := regexp.MustCompile(DontRegex)
+	dore := regexp.MustCompile(DoRegex)
+	sum := 0
+
+	for i, v := range re.Split(input, -1) {
+		if i == 0 {
+			sum += ProgramInterpreter(v)
+		} else {
+			for _, k := range dore.Split(v, -1)[1:] {
+				sum += ProgramInterpreter(k)
+			}
+		}
+	}
+	return sum
 }
