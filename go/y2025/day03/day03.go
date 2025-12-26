096305b11138075134e3688f82fe078d18ff2e13
cpmachado <cpmachado@protonmail.com>
2025-12-26 23:41:13 +0000

    part1 green test

--- go/y2025/day03/day03.go
+++ go/y2025/day03/day03.go
@@ -11,7 +11,35 @@ func Process(r io.Reader, part2 bool) (int, error) {
 	sum := 0
 
 	for scanner.Scan() {
+		token := scanner.Text()
+		mj := retrieveMaxJoltage(token)
+		sum += mj
 	}
 
 	return sum, nil
+}
+
+func retrieveMaxJoltage(s string) int {
+	mdidx := 0
+	ba := '0'
+	n := len(s)
+
+	for i, r := range s {
+		if r > ba && i+1 < n {
+			mdidx = i
+			ba = r
+		}
+	}
+
+	bb := '0'
+
+	for i, r := range s[mdidx+1:] {
+		if r > bb && i+1 < n {
+			bb = r
+		}
+	}
+
+	a := int(ba - '0')
+	b := int(bb - '0')
+	return a*10 + b
 }
