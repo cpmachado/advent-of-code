c648eddea7f25d65b2f2cf1968bbd7f395807b63
cpmachado <cpmachado@protonmail.com>
2025-12-26 23:26:03 +0000

    Inject day03 in cmds

--- go/y2025/days.go
+++ go/y2025/days.go
@@ -4,9 +4,11 @@ import (
 	"github.com/cpmachado/advent-of-code/go/util"
 	"github.com/cpmachado/advent-of-code/go/y2025/day01"
 	"github.com/cpmachado/advent-of-code/go/y2025/day02"
+	"github.com/cpmachado/advent-of-code/go/y2025/day03"
 )
 
 var DayCommands = map[int]util.Command{
 	1: day01.Command,
 	2: day02.Command,
+	3: day03.Command,
 }
