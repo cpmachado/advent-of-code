df8feb96e8a93ab38b514d2de6c933d5967d65c8
cpmachado <cpmachado@protonmail.com>
2025-12-26 19:12:32 +0000

    Inject command

---
+++ go/y2025/day02/cmd.go
@@ -0,0 +1,26 @@
+package day02
+
+import (
+	"fmt"
+	"os"
+)
+
+func Command(args []string, second bool) error {
+	if len(args) != 1 {
+		return fmt.Errorf("day02 requires a filename")
+	}
+	filename := args[0]
+
+	file, err := os.Open(filename)
+	if err != nil {
+		return err
+	}
+
+	sum, err := Process(file, second)
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("Sum: %d\n", sum)
+	return nil
+}
--- go/y2025/days.go
+++ go/y2025/days.go
@@ -3,8 +3,10 @@ package y2025
 import (
 	"github.com/cpmachado/advent-of-code/go/util"
 	"github.com/cpmachado/advent-of-code/go/y2025/day01"
+	"github.com/cpmachado/advent-of-code/go/y2025/day02"
 )
 
 var DayCommands = map[int]util.Command{
 	1: day01.Command,
+	2: day02.Command,
 }
