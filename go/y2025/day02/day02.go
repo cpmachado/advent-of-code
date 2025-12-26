239d0b02b72910f7af7dc7ca5d71ea83ce5eb265
cpmachado <cpmachado@protonmail.com>
2025-12-26 22:46:01 +0000

    part 1 green

--- go/y2025/day02/day02.go
+++ go/y2025/day02/day02.go
@@ -3,13 +3,54 @@ package day02
 import (
 	"bufio"
 	"io"
+	"strconv"
+	"strings"
 )
 
 func Process(r io.Reader, part2 bool) (int, error) {
 	scanner := bufio.NewScanner(r)
+	scanner.Split(func(data []byte, atEOF bool) (advance int, token []byte, err error) {
+		if atEOF {
+			return len(data), data, io.EOF
+		}
+
+		for i, r := range data {
+			if r == ',' {
+				return i + 1, data[:i], nil
+			}
+		}
+		return len(data), data, nil
+	})
+
+	sum := 0
 
 	for scanner.Scan() {
+		s := strings.Split(scanner.Text(), "-")
+		a, err := strconv.Atoi(s[0])
+		if err != nil {
+			return 0, err
+		}
+		b, err := strconv.Atoi(s[1])
+		if err != nil {
+			return 0, err
+		}
+		for a <= b {
+			sa := strconv.Itoa(a)
+			k := len(sa)
+
+			if k%2 != 0 {
+				a++
+				continue
+			}
+
+			mid := k / 2
+			if sa[:mid] == sa[mid:] {
+				sum += a
+			}
+
+			a++
+		}
 	}
 
-	return 0, scanner.Err()
+	return sum, nil
 }
