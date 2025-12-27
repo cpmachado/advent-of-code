d7dfa83541b406b6f514faec72ee5ea167c354f3
cpmachado <cpmachado@protonmail.com>
2025-12-27 22:38:27 +0000

    part2: red test

--- go/y2025/day07/day07_test.go
+++ go/y2025/day07/day07_test.go
@@ -23,19 +23,33 @@ const sample = `.......S.......
 ...............
 `
 
-func TestPart1(t *testing.T) {
-	// given
-	r := strings.NewReader(sample)
-	want := 21
-
-	// when
-	got, err := Process(r, false)
-	// then
-	if err != nil {
-		t.Fatal(err)
+func TestProcess(t *testing.T) {
+	tests := []struct {
+		description string
+		want        int
+		part2       bool
+	}{
+		{description: "part1", want: 21},
+		{description: "part2", want: 40, part2: true},
 	}
 
-	if got != want {
-		t.Fatalf("Expected %d, but got %d\n", want, got)
+	for _, test := range tests {
+		t.Run(test.description, func(t *testing.T) {
+			// given
+			want := test.want
+			part2 := test.part2
+			r := strings.NewReader(sample)
+
+			// when
+			got, err := Process(r, part2)
+			// then
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			if got != want {
+				t.Fatalf("Expected %d, but got %d\n", want, got)
+			}
+		})
 	}
 }
