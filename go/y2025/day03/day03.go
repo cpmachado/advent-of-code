bdfcf222cf13b3f08ba5d2d613ea2a3b3addb74b
cpmachado <cpmachado@protonmail.com>
2025-12-27 00:00:50 +0000

    Refactor to use general solution of part2

--- go/y2025/day03/day03.go
+++ go/y2025/day03/day03.go
@@ -10,48 +10,22 @@ func Process(r io.Reader, part2 bool) (int, error) {
 
 	sum := 0
 
-	summer := retrieveMaxJoltage
+	k := 2
 
 	if part2 {
-		summer = retrieveMaxJoltagePart2
+		k = 12
 	}
 
 	for scanner.Scan() {
 		token := scanner.Text()
-		mj := summer(token)
+		mj := retrieveMaxJoltage(token, k)
 		sum += mj
 	}
 
 	return sum, nil
 }
 
-func retrieveMaxJoltage(s string) int {
-	mdidx := 0
-	ba := '0'
-	n := len(s)
-
-	for i, r := range s {
-		if r > ba && i+1 < n {
-			mdidx = i
-			ba = r
-		}
-	}
-
-	bb := '0'
-
-	for i, r := range s[mdidx+1:] {
-		if r > bb && i+1 < n {
-			bb = r
-		}
-	}
-
-	a := int(ba - '0')
-	b := int(bb - '0')
-	return a*10 + b
-}
-
-func retrieveMaxJoltagePart2(s string) int {
-	k := 12
+func retrieveMaxJoltage(s string, k int) int {
 	n := len(s)
 	b := 0
 	sum := 0
