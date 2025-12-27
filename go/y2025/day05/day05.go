c322cee40e4e5e38edb5dba8c38f371cf667e6b8
cpmachado <cpmachado@protonmail.com>
2025-12-27 14:26:28 +0000

    part1: green test

--- go/y2025/day05/day05.go
+++ go/y2025/day05/day05.go
@@ -2,22 +2,61 @@ package day05
 
 import (
 	"bufio"
-	"fmt"
 	"io"
+	"strconv"
+	"strings"
 )
+
+type Set struct {
+	A, B int
+}
+
+func (s *Set) Contains(k int) bool {
+	return s.A <= k && k <= s.B
+}
 
 func Process(r io.Reader, part2 bool) (int, error) {
 	scanner := bufio.NewScanner(r)
 
 	count := 0
+	var sets []Set
 
-	var papermap []string
-
+	// parse ranges
 	for scanner.Scan() {
 		token := scanner.Text()
-		papermap = append(papermap, token)
-		fmt.Printf("%q\n", token)
+
+		if token == "" {
+			break
+		}
+
+		ns := strings.Split(token, "-")
+		r := Set{}
+
+		r.A, _ = strconv.Atoi(ns[0])
+		r.B, _ = strconv.Atoi(ns[1])
+
+		sets = append(sets, r)
+	}
+
+	// parse igredients
+	for scanner.Scan() {
+		token := scanner.Text()
+
+		i, _ := strconv.Atoi(token)
+
+		if isFresh(sets, i) {
+			count++
+		}
 	}
 
 	return count, nil
+}
+
+func isFresh(sets []Set, i int) bool {
+	for _, s := range sets {
+		if s.Contains(i) {
+			return true
+		}
+	}
+	return false
 }
