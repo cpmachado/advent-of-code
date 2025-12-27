7885f0d4f045976ed4dc0b907db469c4ea68ccea
cpmachado <cpmachado@protonmail.com>
2025-12-27 02:34:56 +0000

    part2 red test

--- go/y2025/day04/day04_test.go
+++ go/y2025/day04/day04_test.go
@@ -33,3 +33,20 @@ func TestPart1(t *testing.T) {
 		t.Fatalf("Expected %d, but got %d\n", want, got)
 	}
 }
+
+func TestPart2(t *testing.T) {
+	// given
+	r := strings.NewReader(sample)
+	want := 43
+
+	// when
+	got, err := Process(r, true)
+	// then
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if got != want {
+		t.Fatalf("Expected %d, but got %d\n", want, got)
+	}
+}
