03aaba7c0438e514fb29a978a2b2cb595323fcfe
cpmachado <cpmachado@protonmail.com>
2025-12-27 16:47:24 +0000

    Add command

---
+++ go/y2025/day06/cmd.go
@@ -0,0 +1,26 @@
+package day06
+
+import (
+	"fmt"
+	"os"
+)
+
+func Command(args []string, second bool) error {
+	if len(args) != 1 {
+		return fmt.Errorf("day06 requires a filename")
+	}
+	filename := args[0]
+
+	file, err := os.Open(filename)
+	if err != nil {
+		return err
+	}
+
+	sum, err := Process(file, second)
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("sum: %d\n", sum)
+	return nil
+}
