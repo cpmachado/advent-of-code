7b7903e057683ac65caf6ad932759b5a8b0e8f31
cpmachado <cpmachado@protonmail.com>
2025-12-27 02:27:35 +0000

    part1: green test

--- go/y2025/day04/day04.go
+++ go/y2025/day04/day04.go
@@ -8,14 +8,44 @@ import (
 func Process(r io.Reader, part2 bool) (int, error) {
 	scanner := bufio.NewScanner(r)
 
-	sum := 0
+	count := 0
 
-	var paperMap []string
+	var papermap []string
 
 	for scanner.Scan() {
 		token := scanner.Text()
-		paperMap = append(paperMap, token)
+		papermap = append(papermap, token)
 	}
 
-	return sum, nil
+	for i, row := range papermap {
+		for j, r := range row {
+			if r == '@' {
+				if k := countAdjacentRolls(i, j, papermap); k < 4 {
+					count++
+				}
+			}
+		}
+	}
+
+	return count, nil
+}
+
+func countAdjacentRolls(x, y int, papermap []string) int {
+	count := 0
+	for i := -1; i < 2; i++ {
+		if x+i < 0 || x+i >= len(papermap) {
+			continue
+		}
+		row := papermap[x+i]
+		ni := len(row)
+		for j := -1; j < 2; j++ {
+			if y+j < 0 || y+j >= ni || i == 0 && j == 0 {
+				continue
+			}
+			if row[y+j] == '@' {
+				count++
+			}
+		}
+	}
+	return count
 }
